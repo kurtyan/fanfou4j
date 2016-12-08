739302f8822688b3604df86bc8e9edad119be5bf
kurtyan <kurtyan@foxmail.com>
2016-12-08 23:35:36 +0800

    - Implemented multiple friend related apis.

--- src/main/java/com/github/kurtyan/fanfou4j/request/friend/AcceptFollowRequestRequest.kt
+++ src/main/java/com/github/kurtyan/fanfou4j/request/friend/AcceptFollowRequestRequest.kt
@@ -5,9 +5,9 @@ import com.github.kurtyan.fanfou4j.core.HttpMethod
 import com.github.kurtyan.fanfou4j.entity.User
 
 /**
- * Created by yanke on 2016/12/7.
+ * Created by yanke on 2016/12/8.
  */
-class DenyFollowRequestRequest : AbstractRequest<User>("/friendships/deny.json", HttpMethod.POST) {
+class AcceptFollowRequestRequest : AbstractRequest<User>("/friendships/accept.json", HttpMethod.POST){
 
     var id: String by stringDelegate
 
