f6e203ec7ccbe54318dac2cb060c6622818a504c
kurtyan <kurtyan@foxmail.com>
2016-12-16 23:32:03 +0800

    - Implemented create block and destroy block request

---
+++ src/main/java/com/github/kurtyan/fanfou4j/request/block/BlockUserRequest.kt
@@ -0,0 +1,16 @@
+package com.github.kurtyan.fanfou4j.request.block
+
+import com.github.kurtyan.fanfou4j.core.AbstractRequest
+import com.github.kurtyan.fanfou4j.core.HttpMethod
+import com.github.kurtyan.fanfou4j.entity.User
+import com.github.kurtyan.fanfou4j.request.RequestMode
+
+/**
+ * Created by yanke on 2016/12/15.
+ */
+class BlockUserRequest : AbstractRequest<User>("/blocks/create.json", HttpMethod.POST) {
+
+    var id: String by stringDelegate
+    var mode: RequestMode by requestModeDelegate
+
+}
