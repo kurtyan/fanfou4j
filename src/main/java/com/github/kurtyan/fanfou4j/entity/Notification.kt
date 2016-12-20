85453b3979c60fa41de076e1a064264ad061efd6
kurtyan <kurtyan@foxmail.com>
2016-12-20 23:06:46 +0800

    - Implemented show notifications api.

---
+++ src/main/java/com/github/kurtyan/fanfou4j/entity/Notification.kt
@@ -0,0 +1,12 @@
+package com.github.kurtyan.fanfou4j.entity
+
+/**
+ * Created by yanke on 2016/12/20.
+ */
+class Notification {
+
+    var mentions: Long? = null
+    var directMessages: Long? = null
+    var friendRequests: Long? = null
+
+}
